91dc63797b6aac710c6a3b8936ddfba0158a6bd2
Reindert <16726304+reindert-vetter@users.noreply.github.com>
2022-11-15 00:31:22 +0100

    Update router.blade.php

--- views/router.blade.php
+++ views/router.blade.php
@@ -1,16 +1,22 @@
 @php
+//  Generate a list with 3 columns for the product overview 
     $page = section('page')->list('active', 'title', 'slug');
+
+//  Define de fields for every page
     $page->text('title')->min(2)->max(300)->required();
     $page->text('uri')->unique()->required();
     $page->checkbox('active');
     $page->select('template')->fromDirectory('views.templates');
 
+//  Get the current page
     $currentPage = $page->where('slug' , request()->slug())->first();
 @endphp
 @if($currentPage && $currentPage->get('active'))
+    // Set the browser tab title. See page.blade.php.
     @section('page_title')
         {{ $currentPage->get('title') }}
     @endsection
+    // Include the chosen view and allow extra fields
     @include($currentPage->get('template'), ['page', $currentPage])
 @else
     @include('errors.404')

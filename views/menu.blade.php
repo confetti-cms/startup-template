9daa4ae2b202f36eececfdbb983216868b21f974
Reindert Vetter <reindertvetter@gmail.com>
2022-11-15 09:42:41 +0100

    Proposal page routing

--- views/menu.blade.php
+++ views/menu.blade.php
@@ -1,13 +1,11 @@
 @section('top_menu')
     @php($menu = section('menu'))
     <div class="menu">
-        @foreach($menu->multiple('items')->min(2)->max(10)->sortable() as $column)
-            <ul>
-                @foreach($column->multiple('rows')->min(1)->max(5)->sortable() as $item)
-                    @php($link = $item->select('link_to')->fromSection('page'))
-                    <li><a href="{{ $item->get('uri') }}">{{ $item->get('title') }}</a></li>
-                @endforeach
-            </ul>
-        @endforeach
+        <ul>
+            @foreach($column->multiple('items')->min(1)->max(5)->sortable() as $item)
+                @php($link = $item->select('link_to')->fromSection('page'))
+                <li><a href="{{ $item->get('uri') }}">{{ $item->get('title') }}</a></li>
+            @endforeach
+        </ul>
     </div>
 @endsection
